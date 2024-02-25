9d01905f99b6de3b72da5d459a15d831eb7c6716
Jan Tomsa <jan.tomsa@tomsovi.com>
2024-02-25 21:01:14 +0100

    added second unit test

--- TryingGoogleTest1/test.cpp
+++ TryingGoogleTest1/test.cpp
@@ -10,5 +10,11 @@ TEST(TestStringLength, TestLength) {
 	string tst_string = "XXX";
 	int l = length_of_string(tst_string);
 	EXPECT_EQ(l, 3);
+}
+
+TEST(TestStringLength2, TestLength) {
+	string tst_string = "Hello!";
+	int l = length_of_string(tst_string);
+	EXPECT_EQ(l, 6);
 	//EXPECT_TRUE(true);
 }
