472401bee5122b38ce8bad237f61e64a51cffbb9
Jan Tomsa <jan.tomsa@tomsovi.com>
2024-02-25 21:25:42 +0100

    New functionality - playing with vector, new UT

---
+++ TryingGoogleTest1/SplitString.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "pch.h"
+
+std::vector<std::string> split(const std::string& s, char delim);
--- TryingGoogleTest1/test.cpp
+++ TryingGoogleTest1/test.cpp
@@ -2,6 +2,7 @@
 
 #include "pch.h"
 #include "MyUnit1.hpp"
+#include "SplitString.hpp"
 
 using namespace std;
 
@@ -18,3 +19,12 @@ TEST(TestStringLength2, TestLength) {
 	EXPECT_EQ(l, 6);
 	//EXPECT_TRUE(true);
 }
+
+
+TEST(TestStringSplit1, TestSplitting) {
+	string tst_string = "First:Second:Third";
+	auto r = split(tst_string, ':');
+	int l = r.size();
+	EXPECT_EQ(l, 3);
+}
+
